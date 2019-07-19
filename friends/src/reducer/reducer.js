102c5329052bc905997ebca7f7a49b7c739cf8ed
teej387 <teej387@yahoo.com>
2019-07-18 19:50:18 -0500

    imported actions into reducer.js and added some initialState

--- friends/src/reducer/reducer.js
+++ friends/src/reducer/reducer.js
@@ -1,6 +1,16 @@
-
+import {
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  GET_DATA_START,
+  GET_DATA_SUCCESS,
+  GET_DATA_FAILED,
+} from '../actions/action'
 
 const initialState = {
   friends: [],
+  isLoading: false,
+  error: null,
 }
+
 
