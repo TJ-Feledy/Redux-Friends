290c3771de0524ac7c75c186e3b136a38c41e544
teej387 <teej387@yahoo.com>
2019-07-18 19:24:13 -0500

    Converted index.js into a redux compatible component with thunk and applyMiddleware

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,5 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducer/reducer'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, 
+  document.getElementById('root')
+);
