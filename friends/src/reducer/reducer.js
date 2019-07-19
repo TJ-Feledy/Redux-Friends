3025f72f6adbbc775eff9a14349ab3b378bb3170
teej387 <teej387@yahoo.com>
2019-07-18 20:02:19 -0500

    Modified reducer to handle GET_DATA and LOGIN

--- friends/src/reducer/reducer.js
+++ friends/src/reducer/reducer.js
@@ -13,4 +13,51 @@ const initialState = {
   error: null,
 }
 
-
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_DATA_START: {
+      return {
+        ...state,
+        isLoading: true,
+      }
+    }
+    case GET_DATA_SUCCESS: {
+      const { friends } = action.payload
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        friends,
+      }
+    }
+    case GET_DATA_FAILED: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.msg,
+      }
+    }
+    case LOGIN_START: {
+      return {
+        ...state,
+        isLoading: true,
+      }
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+      }
+    }
+    case LOGIN_FAILED: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.msg,
+      }
+    }
+    default:
+      return state
+  }
+}
