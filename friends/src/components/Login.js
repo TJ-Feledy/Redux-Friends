f5f12ab83e2514767c8b9340c4002af03aac57a1
teej387 <teej387@yahoo.com>
2019-07-18 20:33:27 -0500

    Created Login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { login } from '../actions/action'
+
+class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  handleChange = (evt) => {
+    evt.preventDefault()
+
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    this.props.login(username, password)
+      .then(() => {
+        this.props.history.push('/')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    const { username, password } = this.state
+    const { isLoading, error } = this.props
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {error && <p className='error'>{error}</p>}
+
+        <input type='text' name='username' placeholder='Username' value={username} on Change={this.handleChange} /><br />
+        <input type='password' name='password' placeholder='Password' value={password} on Change={this.handleChange} /><br />
+
+        {isLoading ? <p>Logging in...</p> : <button type='submit'>Login</button>}
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
+  error: state.error,
+})
+
+const mapDispatchToProps = {
+  login,
+}
+
+export default withRouter(
+  connect(
+    mapStateToProps, mapDispatchToProps,
+  )(Login)
+)
